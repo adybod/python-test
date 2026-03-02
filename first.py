880f1387c9dfa0d2d009ff445b688d21291712fa
adybod <adybod@gmx.de>
2026-03-02 10:14:54 +0530

    first update to the python course

---
+++ first.py
@@ -0,0 +1,30 @@
+string = "hello world"
+print(string)
+print(78) #int
+print(4.5) #float
+print(True) #boolean
+
+print("8"+"1") #concatenation
+print(8+1) #addition
+
+#variables
+number = 7.2353253
+name = "python"
+print(name)
+name = "java"
+print(name)
+
+last_name_school = "python" #only '_' allowed in variable name
+l1astname1 = "python" #first charachters cant be numbers
+
+name = "python"
+age = "40"
+print("my name is "+name, "and my age is", age)
+
+
+print(f"my name is {name} and my age is {age}.") #f-string
+
+print("my name {} and my age is {}.".format(name,age))
+
+
+
---
+++ operators.py
@@ -0,0 +1,23 @@
+#operators
+
+a = 10
+b = 20
+
+print(a + b) #addition - sum of a and b
+print(a - b) #subtraction - difference of a and b
+print(a * b) #multiplication - product of a and b
+print(a / b) #division - quotient of a and b
+print(a % b) #modulus - remainder of the division of a and b
+print(a ** b) #exponentiation - a to the power of b
+print(a // b) #floor division - integer division of a and b
+
+
+#comparison operators
+print(a == b) #equal to
+print(a != b) #not equal to
+print(a > b) #greater than
+print(a < b) #less than
+print(a >= b) #greater than or equal to
+print(a <= b) #less than or equal to
+
+
